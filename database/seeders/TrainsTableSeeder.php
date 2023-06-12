7985ca710e95e1881ece071afedcbb74c6cd19c7
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-06-12 20:37:42 +0200

    Riempimento tabella Trains con dati fittizzi usando Faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Hotel;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +14,20 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run( Faker $faker)
     {
-        //
+        for($i = 0; $i < 10 ; $i++) {
+
+            $newHotel = new Hotel();
+            $newHotel->azienda = $faker->company();
+            $newHotel->stazione_di_partenza = $faker->streetAddress();
+            $newHotel->orario_di_partenza = $faker->time();
+            $newHotel->orario_di_arrivo = $faker->time();
+            $newHotel->codice_treno = $faker->regexify('[A-Z]{5}[0-4]{3}');
+            $newHotel->numero_carrozze = $faker->randomNumber(2, false);
+            $newHotel->in_orario = $faker->randomElement([true, false]);
+            $newHotel->cancellato = $faker->randomElement([true, false]);
+            $newHotel->save();
+        }
     }
 }
